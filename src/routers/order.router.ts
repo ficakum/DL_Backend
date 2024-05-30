00926dc82dccc5d619a7b007a9f1ade4ccc45d4e
ficanikolic <filip.nikolic@nistruct.com>
2024-05-30 15:53:03 +0200

    Add order routes

---
+++ src/routers/order.router.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+
+import orderController from "../controllers/order.controller";
+import authenticationMiddleware from "../middlewares/jwt.middleware";
+import authoriationMiddleware from "../middlewares/authorization.middleware";
+import queryMiddleware from "middlewares/query.middleware";
+import { Roles } from "models/user.model";
+
+const orderRouter: Router = Router();
+
+orderRouter.get(
+  "/:id",
+  authenticationMiddleware,
+  authoriationMiddleware([Roles.ADMIN, Roles.CUSTOMER, Roles.VENDOR]),
+  orderController.getOrder
+);
+orderRouter.get(
+  "/",
+  authenticationMiddleware,
+  authoriationMiddleware([Roles.ADMIN, Roles.CUSTOMER, Roles.VENDOR]),
+  queryMiddleware,
+  orderController.getOrders
+);
+orderRouter.post(
+  "/",
+  authenticationMiddleware,
+  authoriationMiddleware([Roles.ADMIN, Roles.CUSTOMER, Roles.VENDOR]),
+  orderController.createOrder
+);
+orderRouter.patch(
+  "/:id",
+  authenticationMiddleware,
+  authoriationMiddleware([Roles.ADMIN, Roles.CUSTOMER, Roles.VENDOR]),
+  orderController.updateOrder
+);
+orderRouter.delete(
+  "/:id",
+  authenticationMiddleware,
+  authoriationMiddleware([Roles.ADMIN, Roles.CUSTOMER, Roles.VENDOR]),
+  orderController.deleteOrder
+);
+
+export default orderRouter;
